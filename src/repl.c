467076c45ac6842c94e988cc038a5c2b3be93f18
LeTorky <24Torky@gmail.com>
2023-03-11 01:39:50 +0200

    -Fixed issues

--- src/repl.c
+++ src/repl.c
@@ -8,13 +8,17 @@ void move_current_index(int left_or_right, int* current_index, int last_index);
 // Function to insert/replace character into character array.
 void insert_or_replace_character(char user_input, char* char_array, int* current_index, int* last_index);
 
+// Function to allocate new memory for new character inputs.
+void add_to_char_array(int add, char user_input, char** char_array, int last_index);
+
 // Interface to Database (REPL).
 void main(){
     // Variables
-    char* user_entry;
+    char* user_entry = (char*)malloc(sizeof(char));
+    *user_entry = '\0';
     int toggle = 1;
     int current_index = 0;
-    int last_index = 0;
+    int last_index = -1;
 
     // Conditional loop to take input from user until exit is written.
     char scanned_char;
@@ -28,27 +32,27 @@ void main(){
                 scanned_char = getch();
                 switch(scanned_char){
 
-                    // Left arrow case.
+                    // Left arrow key case.
                     case 75:
                         move_current_index(0, &current_index, last_index);
                         break;
 
-                    // Right arrow case.
+                    // Right arrow key case.
                     case 77:
                         move_current_index(1, &current_index, last_index);
                         break;
                 }
                 break;
 
-            // Enter character case.
+            // Enter key case.
             case 13:
-                printf("%s", user_entry);
+                printf("\n %s", user_entry);
 
             // Actual input characters.
             default:
                 // Move cursor, print and save character.
+                insert_or_replace_character(scanned_char, user_entry, &current_index, &last_index);
                 printf("%c", scanned_char);
-                insert_or_replace_character(scanned_char, &user_entry, &current_index, &last_index);
                 break;
         }
     }
@@ -61,22 +65,43 @@ void move_current_index(int left_or_right, int* current_index, int last_index){
     if (left_or_right){
         // Increment Index and move cursor to the right.
         if(*current_index <= last_index){
-            ++*current_index;
+            *current_index += 1;
         }
     }
     // Move left
     // Decrement Index and move cursor to the left.
-    if(*current_index > 0){
+    else if(*current_index > 0){
         --*current_index;
     }
 }
 
 void insert_or_replace_character(char user_input, char* char_array, int* current_index, int* last_index){
-    // Add character to character array.
-    char_array[*current_index] = user_input;
     // Move index if a character was inserted.
     if(*current_index > *last_index){
         ++*last_index;
         move_current_index(1, current_index, *last_index);
+        add_to_char_array(1, user_input, &char_array, *last_index);
+    }
+    else{
+        // Replaces character in array.
+        char_array[*current_index] = user_input;
+    }
+}
+
+void add_to_char_array(int add, char user_input, char** char_array, int last_index){
+    if(add){
+        // Allocating a new array with the new size. (+1)
+        char* new_array = (char*)malloc((last_index+1)*sizeof(char));
+        for(int i=0; i<last_index; i++){
+            new_array[i] = (*char_array)[i];
+        }
+        new_array[last_index] = user_input;
+        new_array[last_index+1] = '\0';
+        free(*char_array);
+        *char_array = new_array;
+    }
+    else{
+        // Removing last character.
+
     }
 }
