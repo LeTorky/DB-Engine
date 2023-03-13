76757963b145b111853d2508db1e2c5405dc32f9
LeTorky <24Torky@gmail.com>
2023-03-13 08:57:59 +0200

    -Added comment on REPL function.

--- src/repl.c
+++ src/repl.c
@@ -15,6 +15,7 @@ void insert_or_replace_character(char user_input, char** char_array, int* curren
 // Function to allocate new memory for new character inputs.
 void add_to_char_array(char user_input, char** char_array, int last_index);
 
+// Function to move cursor left or right.
 void move_cursor_left_right(int left_right, COORD* x_y);
 
 // Interface to Database Engine (REPL).
