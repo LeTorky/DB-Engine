1246ce3fd0e5b46a6a53dc367c469428cd281241
LeTorky <24Torky@gmail.com>
2023-04-01 22:30:13 +0200

    -Using struct

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
-#include "repl.h"
-#include <stdio.h>
 #include "repl.c"
-
+#include "tokenizer.c"
+#include <stdio.h>
 void main(){
     //Variables
     char* user_entry;
+    tokenArray token_array;
     user_entry = REPL();
-    printf("\n %s", user_entry);
+    token_array = tokenizer(user_entry);
+    printf("\n");
+    for(int i = 0; i < token_array.size; i++){
+        printf("%s \n", token_array.array[i]);
+    }
 }
